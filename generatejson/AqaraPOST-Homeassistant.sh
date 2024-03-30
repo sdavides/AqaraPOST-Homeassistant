66684466a1d12091c0cf5a4ce1e6ae42756f0d46
sdavides <31100253+sdavides@users.noreply.github.com>
2024-03-30 13:57:05 +0100

    Update AqaraPOST-Homeassistant.sh

--- generatejson/AqaraPOST-Homeassistant.sh
+++ generatejson/AqaraPOST-Homeassistant.sh
@@ -56,9 +56,6 @@ echo "rm -rf /tmp/*tmp_Aqara_G3_nodered.json; rm -rf /tmp/*Aqara_G3_nodered.sh" 
 echo "echo #" >> /tmp/Aqara_G3_nodered.sh
 echo "echo complete, save path /tmp/Aqara_G3_nodered.json" >> /tmp/Aqara_G3_nodered.sh
 echo "echo #" >> /tmp/Aqara_G3_nodered.sh
+echo "rm -rf /tmp/*tmp_Aqara_G3_nodered.json; rm -rf /tmp/*Aqara_G3_nodered.sh ; rm -rf /tmp/AqaraPOST-Homeassistant* " >> /tmp/Aqara_G3_nodered.sh
 
 /tmp/Aqara_G3_nodered.sh
-
-sleep 1
-rm -rf /tmp/Aqara_G3_nodered.sh*
-rm -rf /tmp/AqaraPOST-Homeassistant*
