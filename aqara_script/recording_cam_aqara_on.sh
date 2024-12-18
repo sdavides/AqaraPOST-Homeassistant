7cc913a17d86862d91c90aaaf869907a3e857e4c
sdavides <31100253+sdavides@users.noreply.github.com>
2024-12-18 20:37:53 +0100

    Update recording_cam_aqara_on.sh

--- aqara_script/recording_cam_aqara_on.sh
+++ aqara_script/recording_cam_aqara_on.sh
@@ -2,4 +2,4 @@
 
 killall -9 ffmpeg
 
-ffmpeg -i "rtsp://192.168.1.4:8554/720p" -acodec copy -vcodec copy /media/modem_$(date +"%d-%m-%Y_%H-%M-%S").mp4
+ffmpeg -i "rtsp://192.168.1.4:8554/720p" -acodec copy -vcodec copy /media/cameraG3_$(date +"%d-%m-%Y_%H-%M-%S").mp4
